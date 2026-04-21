acb68daa36b2573db4800a02088a8a72f28af25e
NicNeko <roynicolas80@gmail.com>
2026-04-21 17:44:39 -0400

    Init

--- obj/Debug/NicNekoAdminStuff.AssemblyInfo.cs
+++ obj/Debug/NicNekoAdminStuff.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NicNekoAdminStuff")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70996fdc13885988750554b4ed6fa7a81004c773")]
 [assembly: System.Reflection.AssemblyProductAttribute("NicNekoAdminStuff")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NicNekoAdminStuff")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
